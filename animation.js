5aac650ff33c0ad974c164acabdab839a5bf3b1b
Ahmet Sabuncu <ahmetsabuncu@Ahmets-MacBook-Pro.local>
2022-07-31 13:28:15 -0400

    added and updated animation file

--- animation.js
+++ animation.js
@@ -1,9 +1,9 @@
 function swap_language_image() {
     var current_image = document.getElementById("Image_To_Switch").src;
-    if (current_image == "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/HTML5_Badge.svg/240px-HTML5_Badge.svg.png") {
+    if (current_image == "https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg" ) {
       document.getElementById("Image_To_Switch").src =
       "https://cdn.iconscout.com/icon/free/png-256/css-131-722685.png";
-    } else if (current_image == "https://cdn.iconscout.com/icon/free/png-256/javascript-1-225993.png") {
+    } else if (current_image == "https://cdn.iconscout.com/icon/free/png-256/css-131-722685.png") {
       document.getElementById("Image_To_Switch").src =
       "https://cdn.iconscout.com/icon/free/png-256/javascript-1-225993.png";
     } else {
@@ -13,3 +13,5 @@ function swap_language_image() {
   }
   
   setInterval(swap_language_image, 2000);
+
+ 
